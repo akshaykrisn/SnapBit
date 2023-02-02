5cf3276e00b0cf22df75591f65fb5727d1381a67
Atharva Udavant <17.atharva@gmail.com>
2023-02-02 11:20:56 +0530

    Initial

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:snapbit/screens/index.dart';
+import 'package:snapbit/screens/social.dart';
 
 void main() {
   runApp(const MyApp());
@@ -8,15 +8,17 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
+
       ),
-      home: const IndexPage(),
+      home: SocialPage(),
     );
   }
 }
--- lib/screens/social.dart
+++ lib/screens/social.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter_vector_icons/flutter_vector_icons.dart';
+import 'package:line_icons/line_icon.dart';
 
 class SocialPage extends StatefulWidget {
   const SocialPage({super.key});
@@ -11,15 +12,33 @@ class SocialPage extends StatefulWidget {
 
 class _SocialPageState extends State<SocialPage> {
   static const TextStyle optionStyle =
-      TextStyle(fontSize: 30, fontWeight: FontWeight.w600);
+      TextStyle(fontSize: 30, fontWeight: FontWeight.w600,color: Colors.white);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () {},
+          icon: LineIcon.bell(),
+        ),
+        actions: [
+          CircleAvatar(child: IconButton(onPressed: (){}, icon: Icon(FontAwesome.user)))
+        ],
+      ),
       body: Center(
         child: Text(
           'Explore',
           style: optionStyle,
         ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(icon: Icon(Entypo.home),label:'Home'),
+          BottomNavigationBarItem(icon: Icon(Ionicons.compass),label: 'Social'),
+          BottomNavigationBarItem(icon: Icon(MaterialCommunityIcons.bullseye_arrow),label: 'Challenge'),
+        ],
+        
       ),
     );
   }
