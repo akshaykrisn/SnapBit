5e624ee566090e6764bbe60287868fd5a487b878
Atharva Udavant <17.atharva@gmail.com>
2023-02-02 18:09:25 +0530

    tu kar

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:provider/provider.dart';
+import 'package:snapbit/components/google_sign_in.dart';
 import 'package:snapbit/screens/index.dart';
+import 'package:snapbit/screens/loginScreen.dart';
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  Firebase.initializeApp();
   await Hive.initFlutter();
 
   // open a box
@@ -12,17 +18,38 @@ Future<void> main() async {
 }
 
 class MyApp extends StatelessWidget {
+
+
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const IndexPage(),
+  Widget build(BuildContext context) => ChangeNotifierProvider(
+      create: (context) => GoogleSignInProvider(),
+      child:FutureBuilder(
+        // Initialize FlutterFire
+        future: Firebase.initializeApp(),
+        builder: (context, snapshot) {
+          // Check for errors
+          if (snapshot.hasError) {
+            return  Center(child: Text('Something went wrong'));
+          }
+
+          // Once complete, show your application
+          else if (snapshot.connectionState == ConnectionState.done) {
+            return MaterialApp(
+              title: 'Flutter Demo',
+              debugShowCheckedModeBanner: false,
+              theme: ThemeData(
+                primarySwatch: Colors.blue,
+              ),
+              home: LoginScreen(),
+            );
+          }
+
+          // Otherwise, show something whilst waiting for initialization to complete
+          return Center(child: CircularProgressIndicator(),);
+        },
+      )
     );
   }
-}
+
--- lib/screens/loginScreen.dart
+++ lib/screens/loginScreen.dart
@@ -1,6 +1,14 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+import 'package:provider/provider.dart';
+import 'package:snapbit/components/google_sign_in.dart';
+
+import 'home.dart';
 
 class LoginScreen extends StatefulWidget {
+
   const LoginScreen({Key? key}) : super(key: key);
 
   @override
@@ -8,10 +16,59 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+
+  @override
+  void initState() {
+    FirebaseAuth.instance
+        .userChanges()
+        .listen((User? user) {
+      if (user == null) {
+        print('User is currently signed out!');
+      } else {
+        print('User is signed in!');
+      }
+    });
+
+    super.initState();
+  }
+
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body:Column(
+                children: [
+                  SizedBox(height: MediaQuery.of(context).size.height*0.08,),
+                  Text('Welcome to Snapbit!',style: TextStyle(
+                    fontSize: 30,
+                    color: Colors.white,
+                  ),),
+                  SizedBox(height: MediaQuery.of(context).size.height*0.05,),
+                  Lottie.asset('assets/animations/rebound.json',
+                    width: MediaQuery.of(context).size.width*0.8,
+                    height: MediaQuery.of(context).size.height*0.3
+                  ),
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height*0.4,
+                  ),
+                  Container(
+                    color: Color(0xff1355cd),
+                    height: MediaQuery.of(context).size.height*0.06,
+                    width: MediaQuery.of(context).size.width*0.8,
+                    child: OutlinedButton.icon(
+                      onPressed: () {
+                        final provider = Provider.of<GoogleSignInProvider>(context,listen: false);
+                        provider.googleLogin();
+                      },
+                      label: Text('Login with Google Account',style: TextStyle(color: Colors.white),),
+                      icon: Image(image: AssetImage('assets/g.png'),width: 30,),
+
+                    )
+                  ),
+                ],
+              )
+
     );
   }
 }
